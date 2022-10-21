bea536b5d2d60fe583bfd38c4050428e9edd2b33
mitrakov <mitrakov-artem@yandex.ru>
2022-10-21 16:43:13 +0400

    Initial commit

---
+++ lib/model.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: curly_braces_in_flow_control_structures, avoid_print
+import 'dart:math';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart';
+
+/// Main model class
+class MyModel extends Model {
+  // vals
+  static const String url = "http://mitrakoff.com:2000/music";  // allow insecure "http" in settings!
+  final Random _random = Random(DateTime.now().millisecondsSinceEpoch);
+  final List<String> _playlist = [];
+
+  // getters
+  /// List of file names with extension, without URL-encoding, without any URI paths. Example: ["Queen - Show must go on.mp3"]
+  List<String> get playlist => _playlist;
+
+  /// Loads all data from the web server asynchronously. Should be called once.
+  Future loadAll() async {
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final htmlDoc = parse(response.body);
+      final elements  = htmlDoc.getElementsByTagName("a");
+      _playlist.clear();
+      _playlist.addAll(elements.map((e) => e.innerHtml));
+      _playlist.shuffle(_random);
+      print("Playlist: $_playlist");
+    } else throw Exception("Cannot load from $url");
+  }
+}
