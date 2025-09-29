174b50f410cb228961e3740593207d61a0b96dfd
Tommy-Air <tommy@air.ru>
2025-09-29 15:14:57 +0300

    Build v1.5.1 for iOS and MacOS

---
+++ installer/mac/build-dmg.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# https://github.com/create-dmg/create-dmg
+if ! [ `which create-dmg` ]; then
+    echo "Please install: brew install create-dmg"
+    exit 1
+fi
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage:   $0 version"
+    echo "Example: $0 1.2.3"
+    exit 2
+fi
+
+create-dmg \
+  --volname "Tommy Player Installer" \
+  --volicon "appicon.png" \
+  --background "background.jpg" \
+  --window-size 600 425 \
+  --text-size 16 \
+  --icon-size 100 \
+  --icon "Tommyplayer.app" 185 165 \
+  --app-drop-link 405 165 \
+  "tommyplayer-$1.dmg" \
+  "App/"
