9412b8e4051a0dcb1a68f6ecefce5001aaa1602d
Tommy-Air <tommy@air.ru>
2025-10-06 20:20:02 +0300

    Fix blinking cursor in SettingsWidget

--- lib/settings/settingswidget.dart
+++ lib/settings/settingswidget.dart
@@ -2,15 +2,21 @@
 import 'package:flutter/material.dart';
 import 'package:tommyplayer/settings/settings.dart';
 
-class SettingsWidget extends StatelessWidget {
-  final settings = Settings.local;
+class SettingsWidget extends StatefulWidget {
+  @override
+  State<SettingsWidget> createState() => _SettingsWidgetState();
+}
+
+class _SettingsWidgetState extends State<SettingsWidget> {
+  // widget MUST be stateful! (to avoid bugs with cursor in TextFields)
   final _serverUriCtrl  = TextEditingController(text: Settings.local.serverUri);
   final _scoresFileCtrl = TextEditingController(text: Settings.local.scoresFilename);
   final _minStarsCtrl   = TextEditingController(text: Settings.local.minStarsToPlay.toString());
 
   @override
   Widget build(BuildContext context) {
-    const decor = InputDecoration(border: OutlineInputBorder());
+    const d = InputDecoration(border: OutlineInputBorder());
+    final settings = Settings.local;
     return Scaffold(
       appBar: AppBar(title: const Text("Tommy Player")),
       body: Padding(
@@ -21,20 +27,26 @@ class SettingsWidget extends StatelessWidget {
             Expanded(
               child: Column(mainAxisSize: MainAxisSize.min, crossAxisAlignment: CrossAxisAlignment.start, children: [
                 const Text("Hostname or IP-address:"),
-                TextField(controller: _serverUriCtrl, onChanged: (s) => settings.serverUri = s, decoration: decor),
+                TextField(controller: _serverUriCtrl, onChanged: (s) => settings.serverUri = s, decoration: d),
                 const SizedBox(height: 20),
                 const Text("Scores filename:"),
-                TextField(controller: _scoresFileCtrl, onChanged: (s) => settings.scoresFilename = s, decoration: decor),
+                TextField(controller: _scoresFileCtrl, onChanged: (s) => settings.scoresFilename = s, decoration: d),
                 const SizedBox(height: 20),
                 const Text("Min stars to play 0-5 (0 = play all):"),
-                TextField(
-                  controller: _minStarsCtrl, onChanged: (s) => settings.minStarsToPlay = int.tryParse(s) ?? 0, decoration: decor
-                ),
+                TextField(controller: _minStarsCtrl, onChanged: (s) => settings.minStarsToPlay = int.tryParse(s) ?? 0, decoration: d),
               ]),
             ),
           ],
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _serverUriCtrl.dispose();
+    _scoresFileCtrl.dispose();
+    _minStarsCtrl.dispose();
+    super.dispose();
   }
 }
